988c446ccafb0e166324eff51d5b2f8f103e05e1
mylonasc <mylonas@ibk.baug.ethz.ch>
2019-01-12 21:13:54 +0100

    small change.

--- dailyDialogue.sh
+++ dailyDialogue.sh
@@ -1,11 +1,11 @@
 export BERT_BASE_DIR=../../multi_cased_L-12_H-768_A-12 # The BERT model we are using.
 #export GLUE_DIR=../glue_data
-export DDIAL_DIR=../../ijcnlp_dailydialog # Assumes you have already unzipped the train/test/dev examples. This is to be automated in the future.
+export DDIAL_DIR=../Datasets/ijcnlp_dailydialog # Assumes you have already unzipped the train/test/dev examples. This is to be automated in the future.
 
 python3 run_classifier.py \
   --task_name=ddial \
   --twotext=true \
-  --do_train=true \
+  --do_train=true\
   --do_eval=true \
   --do_lower_case=false \
   --data_dir=$DDIAL_DIR \
@@ -14,9 +14,9 @@ python3 run_classifier.py \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --max_seq_length=60 \
   --train_batch_size=32 \
-  --learning_rate=2e-5 \
+  --learning_rate=1e-5 \
   --num_train_epochs=10.0 \
-  --output_dir=/tmp/mrpc_output/
+  --output_dir=/tmp/ddial_out/
 #python3 run_classifier.py \
 #  --task_name=ddial \
 #  --do_train=true \
